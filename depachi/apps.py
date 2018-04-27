6b9a560290e8faef89c717f4d06fbc7a0b3833e9
Cristian Tamblay <cristian.tamblay@ing.uchile.cl>
2018-04-27 12:35:55 -0300

    Esqueleto de Django

---
+++ depachi/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DepachiConfig(AppConfig):
+    name = 'depachi'
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\crist\Desktop\Tarea3IS\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
