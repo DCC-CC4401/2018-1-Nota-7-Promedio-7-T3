84922b92f283f8eda3fd4a226917faaa2eae408e
Cristian Tamblay <cristian.tamblay@ing.uchile.cl>
2018-05-12 23:42:51 -0400

    Ficha articulo casi perfecta

--- articulos/views.py
+++ articulos/views.py
@@ -6,7 +6,7 @@ from .models import Articulos
 def id_articulo(request, id_articulo):
     articulo = Articulos.objects.get(id_articulo=id_articulo)
     nombre = articulo.nombre_articulo
-    estado = articulo.estado_articulo
+    estado = articulo.ESTADOS_ART[articulo.estado_articulo-1][1]
     descripcion = articulo.descripcion_articulo
     foto = articulo.foto_articulo
     id_art = articulo.id_articulo
