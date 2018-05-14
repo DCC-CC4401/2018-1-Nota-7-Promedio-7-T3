6bb1d21c441e36b88950dce2100f6192526cc557
Adriana <adriana.concha@ing.uchile.cl>
2018-05-14 17:27:32 -0400

    borra people(?)

--- espacios/views.py
+++ espacios/views.py
@@ -6,8 +6,5 @@ from django_tables2   import RequestConfig
 from .tables  import EspaciosTable
 
 
-class EspaciosView(View):
-    def espaciosV():
-        return render( 'grilla_Esp.html.html', {'people': Espacios.objects.all()})
 
 
