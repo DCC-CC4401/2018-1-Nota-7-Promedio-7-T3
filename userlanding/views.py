a253fd59f170a84fd7b574c5cbbcf5f2f2d0622f
Matipedia <maty.llegas@gmail.com>
2018-05-14 16:19:55 -0400

    Busqueda de usuarios mejorada

--- userlanding/views.py
+++ userlanding/views.py
@@ -4,15 +4,18 @@ from articulos.models import Articulos
 
 def busqueda(request):
 
+
+    nombre = ""
     resultados = Articulos.objects.all()
 
     search_name = request.GET.get("nombre")
     if search_name:
+        nombre = search_name
         resultados = resultados.filter(nombre_articulo__icontains=search_name)
 
     search_status = request.GET.get("estado")
     if search_status and search_status != '0':
         resultados = resultados.filter(estado_articulo=search_status)
 
-    context = {'resultados' : resultados}
+    context = {'resultados' : resultados, 'nombre' : nombre}
     return render(request, 'busqueda_articulos.html', context)
