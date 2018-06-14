b738a0303d43432ce770afb61c4fefd5a8aed51a
Matipedia <maty.llegas@gmail.com>
2018-06-14 00:20:36 -0400

    Funcionalidad de articulos por fecha y comienzo articulos con calendario

--- userlanding/views.py
+++ userlanding/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from articulos.models import Articulos
+from reservas.models import ReservaArticulo
+from django.db.models import Q
 
 
 def busqueda(request):
     nombre = ""
     resultados = Articulos.objects.all()
+    reservas = ReservaArticulo.objects.all()
 
     search_name = request.GET.get("nombre")
     if search_name:
@@ -15,7 +18,17 @@ def busqueda(request):
     if search_status and search_status != '0':
         resultados = resultados.filter(estado_articulo=search_status)
 
-    context = {'resultados' : resultados, 'nombre' : nombre}
+    search_date_st = request.GET.get("fecha-inicio")
+    search_date_fn = request.GET.get("fecha-fin")
+
+    if search_date_fn and search_date_st:
+        reservas = reservas.filter(
+            Q(inicio__range=(search_date_st, search_date_fn)) | Q(final__range=(search_date_st, search_date_fn)))
+        for res in reservas:
+            resultados = resultados.exclude(id=res.articulo.id)
+
+    context = {'resultados': resultados, 'nombre': nombre, 'fecha_in': search_date_st, 'fecha_fn': search_date_fn,
+               'reservas': reservas}
     return render(request, 'busqueda_articulos.html', context)
 
 
